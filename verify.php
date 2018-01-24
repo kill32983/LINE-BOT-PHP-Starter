f7903672dfd0739e86f7dead3a46d7879905b391
Jiradet <33411459+kill32983@users.noreply.github.com>
2018-01-24 22:56:17 +0700

    Add files via upload
    edit

--- verify.php
+++ verify.php
@@ -1,8 +1,15 @@
 <?php
+$access_token = '1Ri88DgUgyogY1+e/c+fmWU4ljK/5ep7/+xHmUs+iDKgNJCcj5ulIOWVHCPESJfdB2iiRlA1Wm+okMaa0YY/d7mrYjx1fdITeCOEnXCdEEs/RodJjbdNzha1xfvNq21pAn5FHH0aC4JfCz5bOfHrcwdB04t89/1O/w1cDnyilFU=';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$url = 'https://api.line.me/v1/oauth/verify';
 
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
